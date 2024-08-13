ec5dd796c2ab9f485a176e0b172fabf6d850b503
mzharw <mazhariwirasena@gmail.com>
2024-08-13 19:39:00 +0700

    chore: update config

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,12 @@ const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
 const nextConfig = {
     // Ensure Next.js uses SSG instead of SSR
     // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-    // output: 'export',
+    output: 'standalone',
     // Note: This feature is required to use the Next.js Image component in SSG mode.
     // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
-    // images: {
-    //     unoptimized: true,
-    // },
+    images: {
+        unoptimized: true,
+    },
     // Configure assetPrefix or else the server won't properly resolve your assets.
     assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
