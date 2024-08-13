60856c2f4d0d8f5b91e0cc043e80640c4e44ad89
mzharw <mazhariwirasena@gmail.com>
2024-08-13 19:27:05 +0700

    chore: remove tauri config

--- src/app/dashboard/services/fetch.ts
+++ src/app/dashboard/services/fetch.ts
@@ -1,7 +1,6 @@
 import { Stats } from '@/app/dashboard/contexts/DashboardContext';
 
 export async function fetchStats(): Promise<Stats> {
-  console.log(process.env.NEXT_PUBLIC_API_URL)
   const response = await fetch(`/api/proxy?path=sales/stats`);
   if (!response.ok) {
     throw new Error('Failed to fetch stats');
