0cfc4731a1d322a777b54bb69a17dd36debefc9a
mzharw <mazhariwirasena@gmail.com>
2024-08-13 17:57:58 +0700

    chore: debug

--- src/app/dashboard/services/fetch.ts
+++ src/app/dashboard/services/fetch.ts
@@ -1,6 +1,7 @@
 import { Stats } from '@/app/dashboard/contexts/DashboardContext';
 
 export async function fetchStats(): Promise<Stats> {
+  console.log(process.env)
   const response = await fetch(`/api/proxy?path=sales/stats`);
   if (!response.ok) {
     throw new Error('Failed to fetch stats');
